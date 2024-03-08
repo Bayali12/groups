b8bdd89e9a7714f2b5c6e1fa7c05afb9dfe02ba3
Яхиханов Байали Усманович <yaxixanov_b_qschool@qsoft.ru>
2024-03-08 23:59:43 +0300

    fix filter bug

--- src/helpers/filterGroups.ts
+++ src/helpers/filterGroups.ts
@@ -8,7 +8,7 @@ export const filterData = (
         if (filters.colorFilter !== 'all' && item.avatar_color !== filters.colorFilter) {
             return false;
         }
-        if (filters.privacyFilter !== 'all' && (filters.privacyFilter === 1) !== item.closed) {
+        if (filters.privacyFilter !== 'all' && (Number(filters.privacyFilter) === 1) !== item.closed) {
             return false;
         }
         if (filters.friendsSubscribe && !item.friends) {
